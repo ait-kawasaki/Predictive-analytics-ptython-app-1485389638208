d5edbc11441e4726027cdd0a62589497bb5d6c8f
hjm-kawasaki <hjm.kawasaki@gmail.com>
2017-02-02 11:25:03 +0900

    test8

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import BaseHTTPServer
 import CGIHTTPServer
 import cgitb; cgitb.enable()  ## This line enables CGI error reporting
@@ -8,7 +10,7 @@ port = int(os.getenv('VCAP_APP_PORT'))
 server = BaseHTTPServer.HTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
 server_address = ("", port)
-handler.cgi_directories = ["/"]
+handler.cgi_directories = ["cgi-bin/"]
 
 httpd = server(server_address, handler)
 httpd.serve_forever()
