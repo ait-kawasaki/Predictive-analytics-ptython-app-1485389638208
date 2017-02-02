64773fbd60fccb60ea2dc23ca58888e9109f2384
hjm-kawasaki <hjm.kawasaki@gmail.com>
2017-02-02 11:18:55 +0900

    test7

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
+import BaseHTTPServer
+import CGIHTTPServer
+import cgitb; cgitb.enable()  ## This line enables CGI error reporting
 import os
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
+port = int(os.getenv('VCAP_APP_PORT'))
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", port)
+handler.cgi_directories = ["/"]
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
+# try:
+#   from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+#   from SocketServer import TCPServer as Server
+# except ImportError:
+#   from http.server import SimpleHTTPRequestHandler as Handler
+#   from http.server import HTTPServer as Server
+#
+# # Read port selected by the cloud for our application
+# PORT = int(os.getenv('PORT', 8000))
+# # Change current directory to avoid exposure of control files
+# os.chdir('static')
+#
+# httpd = Server(("", PORT), Handler)
+# try:
+#   print("Start serving at port %i" % PORT)
+#   httpd.serve_forever()
+# except KeyboardInterrupt:
+#   pass
+# httpd.server_close()
 
